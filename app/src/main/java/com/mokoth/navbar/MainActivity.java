a9a3bffbbae7714ea8b0bdd922b160cc6bfa791c
moses okoth <jackmokn@gmail.com>
2022-04-03 13:49:44 +0300

    Initial commit

---
+++ app/src/main/java/com/mokoth/navbar/MainActivity.java
@@ -0,0 +1,46 @@
+package com.mokoth.navbar;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+    BottomNavigationView bottomNavigationView;
+    fragment_first firstFragment = new fragment_first();
+    fragment_second secondFragment = new fragment_second();
+    fragment_third thirdFragment = new fragment_third();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setOnNavigationItemSelectedListener(
+                new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                        switch (menuItem.getItemId()) {
+                            case R.id.person:
+                                getSupportFragmentManager().beginTransaction().replace(R.id.container, firstFragment).commit();
+                                return true;
+
+                            case R.id.home:
+                                getSupportFragmentManager().beginTransaction().replace(R.id.container, secondFragment).commit();
+                                return true;
+
+                            case R.id.settings:
+                                getSupportFragmentManager().beginTransaction().replace(R.id.container, thirdFragment).commit();
+                                return true;
+                        }
+                        return false;
+                    }
+                });
+        bottomNavigationView.setSelectedItemId(R.id.person);
+    }
+
+
+
+}
